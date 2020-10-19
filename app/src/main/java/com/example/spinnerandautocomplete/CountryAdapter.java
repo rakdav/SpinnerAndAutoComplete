e72b9c7146f716d0d609771268d41bd7193194f5
rakdav <rakdav@inbox.ru>
2020-10-19 12:04:10 +0200

    3

--- app/src/main/java/com/example/spinnerandautocomplete/CountryAdapter.java
+++ app/src/main/java/com/example/spinnerandautocomplete/CountryAdapter.java
@@ -1,9 +1,14 @@
 package com.example.spinnerandautocomplete;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +16,11 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 
 public class CountryAdapter extends ArrayAdapter<CountryInfo> {
-    private Context context;
+    private Activity context;
     private ArrayList<CountryInfo> Data;
-    public CountryAdapter(@NonNull Context context, int resource, ArrayList<CountryInfo> d) {
+    public CountryAdapter(@NonNull Activity context, int resource, ArrayList<CountryInfo> d) {
         super(context, resource);
+        this.context=context;
         Data=d;
     }
 
@@ -28,7 +34,18 @@ public class CountryAdapter extends ArrayAdapter<CountryInfo> {
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View row=convertView;
         if(row==null){
-
+            LayoutInflater inflater=context.getLayoutInflater();
+            row=inflater.inflate(R.layout.spinner_item,parent,false);
+        }
+        CountryInfo item=Data.get(position);
+        if(item!=null)
+        {
+            ImageView myFlag=row.findViewById(R.id.countryFlag);
+            TextView myCountry=row.findViewById(R.id.contryName);
+            TextView myPopulation=row.findViewById(R.id.countryPopulation);
+            myFlag.setBackground(context.getResources().getDrawable(item.getCountryFlag()));
+            myCountry.setText(item.getCountryName());
+            myPopulation.setText(Long.toString(item.getCountryPopulation()));
         }
         return row;
     }
--- app/src/main/java/com/example/spinnerandautocomplete/MainActivity.java
+++ app/src/main/java/com/example/spinnerandautocomplete/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.spinnerandautocomplete;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        String[] cities={"Moscow","New York","Paris","Berlin"};
-//        Spinner spinner=findViewById(R.id.SpinnerCustom);
+        Spinner spinner=findViewById(R.id.SpinnerCustom);
 //        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,cities);
 //        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //        spinner.setAdapter(adapter);
+        ArrayList<CountryInfo> list=new ArrayList<>();
+        list.add(new CountryInfo(R.drawable.gb,4500000,"Great Britain"));
+        list.add(new CountryInfo(R.drawable.russia,14500000,"Russia"));
+        list.add(new CountryInfo(R.drawable.germany,1500000,"German"));
+        CountryAdapter countryAdapter=new CountryAdapter(this,android.R.layout.simple_spinner_item,list);
+        spinner.setAdapter(countryAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
